2e206a46c83e6737a97e304e6155693054161794
Neel Smith <dnsmith.neel@gmail.com>
2015-04-14 16:31:47 -0400

    Add API docs

--- src/main/groovy/org/homermultitext/utils/HmtDefs.groovy
+++ src/main/groovy/org/homermultitext/utils/HmtDefs.groovy
@@ -1,10 +1,21 @@
 package org.homermultitext.utils
 
+
+/** Class defining some convenience variables for referring to Unicode
+ * code points.
+ */
 class HmtDefs {
 
-    static int highstop = 903
-    static int doubledagger = 8225
-    static int anotherHighStop = 183
-    static ArrayList puncChars = [highstop,doubledagger, anotherHighStop]
+  /** Greek high stop, hex x387. */
+  static int highstop = 903
+
+  /** Latin mid-dot, xB7 */
+  static int anotherHighStop = 183
+
+  /** Double dagger, used in HMT for ... x2021 */
+  static int doubledagger = 8225
+
+  /** Punctuation characters defined in this class. */
+  static ArrayList puncChars = [highstop,doubledagger, anotherHighStop]
 
 }
