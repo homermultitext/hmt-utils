7b90eb2a85e6596e44a619652a7f0644be9e5b7c
Neel Smith <dnsmith.neel@gmail.com>
2015-04-16 14:05:42 -0400

    Rename tokenization classes

---
+++ src/main/groovy/org/homermultitext/utils/HmtDiplomaticTokenization.groovy
@@ -0,0 +1,20 @@
+package org.homermultitext.utils
+
+import org.apache.commons.io.FilenameUtils
+import edu.harvard.chs.f1k.GreekNode
+
+import edu.holycross.shot.greekutils.GreekString
+import edu.holycross.shot.greekutils.MilesianString
+
+
+/** Class to tokenize text following Homer Multitext project conventions for
+*   definition of character set and markup.
+*/
+class HmtDiplomaticTokenization {
+
+  
+  /** Empty constructor.
+   */
+  HmtDiplomaticTokenization() {
+  }
+}
