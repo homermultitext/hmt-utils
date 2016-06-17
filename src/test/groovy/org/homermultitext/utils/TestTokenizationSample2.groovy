ac7bc3ee7154984b8736637e263b8d96b44edff3
Neel Smith <dnsmith.neel@gmail.com>
2016-06-17 11:23:56 -0400

    Correct variable name

--- src/test/groovy/org/homermultitext/utils/TestTokenizationSample2.groovy
+++ src/test/groovy/org/homermultitext/utils/TestTokenizationSample2.groovy
@@ -4,27 +4,42 @@ package org.homermultitext.utils
 import static org.junit.Assert.*
 import org.junit.Test
 
+
+import edu.harvard.chs.cite.CtsUrn
+import edu.harvard.chs.cite.CiteUrn
+
 class TestTokenizationSample2 extends GroovyTestCase {
 
+
+  // Real-world sample of complex markup from Iliadic text
   File tabsDir = new File("testdata/tabs/")
   File tabSrc = new File(tabsDir, "Iliad-small.txt")
-  File outputFile = new File("testdata/out/tokens.txt")
   String separatorStr = "#"
 
 
   void testEditorialTokenizer() {
     HmtEditorialTokenization toker = new HmtEditorialTokenization()
-    def tokenizationResults = toker.tokenizeTabFile(tabSrc,separatorStr)
-
-
+    def analyses = toker.tokenizeTabFile(tabSrc,separatorStr)
 
     // test size of output:
     Integer expectedTokens = 70
     assert tokenizationResults.size() == expectedTokens
-    println "Tokens = " + tokenizationResults.size()
-    tokenizationResults.each {
-      println it
+
+    String expectedNs = "hmt"
+    def expectedCollections = ["tokentypes", "pers", "peoples"]
+    analyses.each { toke ->
+      try {
+        CtsUrn urn = new CtsUrn(toke[0])
+        CiteUrn analysisUrn = new CiteUrn(toke[1])
+        assert analysisUrn.getNs() == expectedNs
+        assert expectedCollections.contains(analysisUrn.getCollection())
+
+      } catch (Exception e) {
+        System.err.println("Failed on URN " + toke[0])
+        println e.toString()
+      }
     }
+
   }
 
 }
